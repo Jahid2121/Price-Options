2d6cb24cce74b1175cb8a30b5afd83be61ab21d4
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-18 16:26:40 +0600

    Make 3 card and make them responsive

--- src/component/PriceOption/PriceOption.jsx
+++ src/component/PriceOption/PriceOption.jsx
@@ -1,14 +1,21 @@
 import PropTypes from 'prop-types';
+import Feature from '../Feature/Feature';
 
 const PriceOption = ({option}) => {
     const {name, price, features} = option
     return (
-        <div className='bg-blue-500 rounded-md text-white p-4'>
+        <div className='bg-blue-500 rounded-md flex flex-col  text-white p-4'>
            <h2 className='text-center'>
             <span className='text-7xl font-extrabold'>{price}</span>
             <span className='text-2xl'>/mon</span>
            </h2>
-           <h4 className='text-3xl '>{name}</h4>
+           <h4 className='text-3xl text-center my-8 '>{name}</h4>
+           <div className='pl-6 flex-grow'>
+           {
+            features.map((feature, idx) => <Feature key={idx} feature={feature}/>)
+           }
+           </div>
+           <button className='mt-12 w-full btn btn-blue-dark py-2 hover:bg-blue-200'>Buy Now</button>
         </div>
     );
 };
